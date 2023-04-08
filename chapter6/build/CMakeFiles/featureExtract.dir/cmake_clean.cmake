83272ba57a84415e5e586ea6063b4a9870829a75
Ari Nguyen <quynhnhu3132000@gmail.com>
2023-04-08 20:56:19 +0000

    add output images

---
+++ chapter6/build/CMakeFiles/featureExtract.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/featureExtract.dir/main.cpp.o"
+  "CMakeFiles/featureExtract.dir/utils/MultipleImageWindow.cpp.o"
+  "featureExtract.pdb"
+  "featureExtract"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/featureExtract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
